60a4a9b81a7bb888021993b6e7ae7fba7709fc8d
marineja <marine.jalabert@laposte.net>
2021-03-24 18:00:20 +0100

    fonction recherche des articles et rajout des noms des camera en html grace a js

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,32 @@
-alert ("site")
+
+
+var allarticles = recherchedinfocamera();
+console.log(allarticles);
+var allarticleJSON = JSON.parse(allarticles);
+console.log(allarticleJSON);
+
+allarticleJSON.forEach(element => {
+    console.log(element.name);
+    let block = document.createElement("div")
+    block.setAttribute("class","nomarticle")
+    block.innerHTML = element.name;
+    let balisehtmlarticles = document.getElementById("articles");
+    balisehtmlarticles.appendChild(block);
+
+});
+
+
+
+
+function recherchedinfocamera() {
+    var articles;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      articles = this.responseText;
+      }
+    };
+    xhttp.open("GET", "http://localhost:3000/api/cameras", false);
+    xhttp.send();
+    return articles;
+  }
